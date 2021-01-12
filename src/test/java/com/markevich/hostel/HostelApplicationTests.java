1b0da98d31476b786b9d53571c7c7e8876604aee
Sasha Markevich <44451034+HeatLee@users.noreply.github.com>
2021-01-12 15:53:55 +0300

    creating project

---
+++ src/test/java/com/markevich/hostel/HostelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.markevich.hostel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
